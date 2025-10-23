988b823a2256badc7841ee421c6dc1dea2244cc3
Pablo El Maldy <pablojamiro2008@hotmail.com>
2025-10-23 10:28:21 -0500

    feat

--- src/app/api/env/route.ts
+++ src/app/api/env/route.ts
@@ -4,6 +4,8 @@ export async function GET() {
   return NextResponse.json({
     NODE_ENV: process.env.NODE_ENV,
     PIANO_API: process.env.PIANO_API,
-    ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS
+    ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
   })
 }
